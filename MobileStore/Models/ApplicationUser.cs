29a36ece038de4d38ec042593a1e9de3e35f678b
Unknown <nguyentrisinh0810@gmail.com>
2017-11-20 18:03:23 +0700

    Init project

---
+++ MobileStore/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace MobileStore.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
