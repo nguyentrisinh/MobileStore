b63ed157987fed762c52091af3d603732e10323d
Unknown <nguyentrisinh0810@gmail.com>
2017-11-28 23:58:18 +0700

    test

--- MobileStore/Migrations/20171128165238_Fix_ModelForeignKeyBrandID_Model.Designer.cs
+++ MobileStore/Migrations/20171128165238_Fix_ModelForeignKeyBrandID_Model.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace MobileStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171128165238_Fix_ModelForeignKeyBrandID_Model")]
+    partial class Fix_ModelForeignKeyBrandID_Model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MobileStore/Migrations/20171128165238_Fix_ModelForeignKeyBrandID_Model.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MobileStore.Migrations
+{
+    public partial class Fix_ModelForeignKeyBrandID_Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Model_Brand_ModelID",
+                table: "Model");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModelID",
+                table: "Model",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Model_BrandID",
+                table: "Model",
+                column: "BrandID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Model_Brand_BrandID",
+                table: "Model",
+                column: "BrandID",
+                principalTable: "Brand",
+                principalColumn: "BrandID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Model_Brand_BrandID",
+                table: "Model");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Model_BrandID",
+                table: "Model");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModelID",
+                table: "Model",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Model_Brand_ModelID",
+                table: "Model",
+                column: "ModelID",
+                principalTable: "Brand",
+                principalColumn: "BrandID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
