5d28d760af7f88b7c1939c56d27118b037d924e6
Unknown <nguyentrisinh0810@gmail.com>
2017-12-02 00:07:16 +0700

    CRUD for ApplicationUser with Create Role (Edit not update role yet)

--- MobileStore/Controllers/ApplicationUsersController.cs
+++ MobileStore/Controllers/ApplicationUsersController.cs
@@ -7,20 +7,44 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MobileStore.Data;
 using MobileStore.Models;
+using MobileStore.Models.ApplicationUserViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using MobileStore.Services;
+using Microsoft.Extensions.Logging;
+
 
 namespace MobileStore.Controllers
 {
     public class ApplicationUsersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger _logger;
 
-        public ApplicationUsersController(ApplicationDbContext context)
+        public ApplicationUsersController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ILogger<AccountController> logger,
+            ApplicationDbContext context
+            )
         {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
             _context = context;
         }
 
-        // GET: ApplicationUsers
-        public async Task<IActionResult> Index()
+            // GET: ApplicationUsers
+            public async Task<IActionResult> Index()
         {
             return View(await _context.ApplicationUser.ToListAsync());
         }
@@ -54,18 +78,41 @@ namespace MobileStore.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FirstName,LastName,Birthday,Phone,Address,Role,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
+        public async Task<IActionResult> Create([Bind("Email,Password,ConfirmPassword,FirstName,LastName,Birthday,Phone,Address,Role")] CreateApplicationUserViewModel model)
         {
+
             if (ModelState.IsValid)
             {
-                _context.Add(applicationUser);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var user = ConvertCreateApplicationUserViewModelToApplicationUserModel(model);
+                //var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var isCreatedUserRole = await CreateNewUserRole(user, model.Role);
+
+                    if (isCreatedUserRole)
+                    {
+                        _logger.LogInformation("Create UserRole successfully");
+                    }
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+
+                    _logger.LogInformation("User created a new account with password.");
+                    return RedirectToAction(nameof(Index));
+                }
+
+                AddErrors(result);
             }
-            return View(applicationUser);
+
+            return View(model);
         }
 
-        // GET: ApplicationUsers/Edit/5
+        #region GET: ApplicationUsers/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
             if (id == null)
@@ -80,13 +127,14 @@ namespace MobileStore.Controllers
             }
             return View(applicationUser);
         }
+        #endregion
 
-        // POST: ApplicationUsers/Edit/5
+        #region POST: ApplicationUsers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,Birthday,Phone,Address,Role,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser applicationUser)
+        public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,Birthday,Phone,Address,Role,Id,Email,ConcurrencyStamp")] ApplicationUser applicationUser)
         {
             if (id != applicationUser.Id)
             {
@@ -115,8 +163,9 @@ namespace MobileStore.Controllers
             }
             return View(applicationUser);
         }
+        #endregion
 
-        // GET: ApplicationUsers/Delete/5
+        #region GET: ApplicationUsers/Delete/5
         public async Task<IActionResult> Delete(string id)
         {
             if (id == null)
@@ -133,8 +182,9 @@ namespace MobileStore.Controllers
 
             return View(applicationUser);
         }
+        #endregion
 
-        // POST: ApplicationUsers/Delete/5
+        #region POST: ApplicationUsers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
@@ -144,10 +194,102 @@ namespace MobileStore.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        #endregion
 
+
+        #region Support Function for ApplicationUserController
         private bool ApplicationUserExists(string id)
         {
             return _context.ApplicationUser.Any(e => e.Id == id);
         }
+
+        #region Convert Models
+        private ApplicationUser ConvertCreateApplicationUserViewModelToApplicationUserModel(CreateApplicationUserViewModel model)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Birthday = model.Birthday,
+                Phone = model.Phone,
+                PhoneNumber = model.Phone,
+                Address = model.Address,
+                Role = model.Role,
+            };
+
+            return user;
+        }
+
+
+        #endregion
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+
+        private async Task NewUserRole(ApplicationUser user, string role)
+        {
+            // Add role to the AspRole table
+            // Check if the role is exists
+            if (!await _roleManager.RoleExistsAsync(role))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            // Add current user to role 
+            await _userManager.AddToRoleAsync(user, "Admin");
+        }
+
+        private async Task<bool> CreateNewUserRole(ApplicationUser user, UserRole role)
+        {
+            try
+            {
+                switch (role)
+                {
+                    case UserRole.Admin:
+                        await NewUserRole(user, "Admin");
+                        break;
+                    case UserRole.WarehouseManager:
+                        await NewUserRole(user, "WarehoustManager");
+                        break;
+                    case UserRole.Technical:
+                        await NewUserRole(user, "Technical");
+                        break;
+                    case UserRole.Accountant:
+                        await NewUserRole(user, "Accountant");
+                        break;
+                    case UserRole.Sale:
+                        await NewUserRole(user, "Sale");
+                        break;
+                }
+
+                return true;
+
+            } catch(Exception ex)
+            {
+                _logger.LogInformation(ex.Message);
+                return false;
+            }
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+        #endregion
+
     }
 }
---
+++ MobileStore/Models/ApplicationUserViewModels/EditApplicationUserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MobileStore.Models.ApplicationUserViewModels
+{
+    public class EditApplicationUserViewModel
+    {
+        #region ApplicationUser Identity Attribute
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        #endregion
+
+        #region ApplicationUser info
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public UserRole Role { get; set; }
+        #endregion
+    }
+}
