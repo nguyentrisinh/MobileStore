0c84868386476e72d9dd356f9626595356f05e66
Sinhnt <nguyentrisinh0810@gmail.com>
2017-12-10 20:11:07 +0700

    Add TransactionCode to WarrantyCard

--- MobileStore/Migrations/20171210131026_Add_TransactionCode_WarrantyCard.Designer.cs
+++ MobileStore/Migrations/20171210131026_Add_TransactionCode_WarrantyCard.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace MobileStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171210131026_Add_TransactionCode_WarrantyCard")]
+    partial class Add_TransactionCode_WarrantyCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
