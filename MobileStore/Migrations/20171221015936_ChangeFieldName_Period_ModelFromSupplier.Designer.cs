b7356180e00544889dc1c497a894dc5ba18aac4d
Unknown <nguyentrisinh0810@gmail.com>
2017-12-21 09:02:34 +0700

    Change Field period to Period (Wrong convention)

--- MobileStore/Migrations/20171221015936_ChangeFieldName_Period_ModelFromSupplier.Designer.cs
+++ MobileStore/Migrations/20171221015936_ChangeFieldName_Period_ModelFromSupplier.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace MobileStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171221015936_ChangeFieldName_Period_ModelFromSupplier")]
+    partial class ChangeFieldName_Period_ModelFromSupplier
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
