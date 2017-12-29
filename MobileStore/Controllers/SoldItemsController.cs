324844e720b17ffe1a32a9d2deebb1446303ac01
Umino <umino.sharp@gmail.com>
2017-12-30 01:59:00 +0700

    Update static solded items

--- MobileStore/Controllers/SoldItemsController.cs
+++ MobileStore/Controllers/SoldItemsController.cs
@@ -20,13 +20,23 @@ namespace MobileStore.Controllers
         // GET: Items
         public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            ViewData["NameParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "name";
-            ViewData["NumberSoldParm"] = sortOrder == "numberSold" ? "numberSold_desc" : "numberSold";
-            ViewData["PriceBoughtParm"] = sortOrder == "priceBought" ? "priceBought_desc" : "priceBought";
-            ViewData["PriceSoldParm"] = sortOrder == "priceSold" ? "priceSold_desc" : "priceSold";
-            ViewData["DiffInPriceParm"] = sortOrder == "diffInPrice" ? "diffInPrice_desc" : "diffInPrice";
-            ViewData["RevenueParm"] = sortOrder == "revenue" ? "revenue_desc" : "revenue";
+            var item = _context.OrderDetail.Include(i => i.Order)
+                        .Include(i => i.Item)
+                        .ThenInclude(i => i.ModelFromSupplier)
+                        .ThenInclude(i => i.StockReceiving)
+                        .ThenInclude(i => i.Supplier);
 
+            item = from i in item group i by i.Item.ModelFromSupplierID into newModel select ;
+
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "name";
+            ViewData["NumberSoldSortParm"] = sortOrder == "numberSold" ? "numberSold_desc" : "numberSold";
+            ViewData["PriceBoughtSortParm"] = sortOrder == "priceBought" ? "priceBought_desc" : "priceBought";
+            ViewData["PriceSoldSortParm"] = sortOrder == "priceSold" ? "priceSold_desc" : "priceSold";
+            ViewData["DiffInPriceSortParm"] = sortOrder == "diffInPrice" ? "diffInPrice_desc" : "diffInPrice";
+            ViewData["RevenueSortParm"] = sortOrder == "revenue" ? "revenue_desc" : "revenue";
+            ViewData["DateSoldSortParm"] = sortOrder == "dateSold" ? "dateSold_desc" : "dateSold";
+
+            // Filtering
             if (searchString != null)
             {
                 page = 1;
@@ -34,71 +44,82 @@ namespace MobileStore.Controllers
                 searchString = currentFilter;
             }
             ViewData["CurrentFilter"] = searchString;
-
-            var item = from m in _context.Item select m;
             if (!String.IsNullOrEmpty(searchString))
             {
-                item = item.Include(i => i.Model).Include(i => i.ModelFromSupplier).Where(i => i.Status == ItemStatus.InStock && i.Name.Contains(searchString));
+                //item = item.Include(i => i.Order)
+                //    .Include(i => i.Item)
+                //    .ThenInclude(i => i.ModelFromSupplier)
+                //    .ThenInclude(i => i.StockReceiving)
+                //    .ThenInclude(i => i.Supplier)
+                //    .Where(i => i.Item.Name.Contains(searchString));
             }
-            switch (sortOrder)
-            {
-                case "name":
-                    //item = item.OrderByDescending(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
 
-                case "name_desc":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            // Tinh so luong ban, chenh lech gia, loi nhuan, doanh thu
+            //var numberSold = from i in item select i.Item.ModelFromSupplierID;
+            //ViewData["NumberSoldParm"] = numberSold.Count();
+            ViewData["DiffInPriceParm"] = sortOrder == "diffInPrice" ? "diffInPrice_desc" : "diffInPrice";
+            ViewData["RevenueParm"] = sortOrder == "revenue" ? "revenue_desc" : "revenue";
 
-                case "numberSold":
+            return View(await item.ToListAsync());
+            //switch (sortOrder)
+            //{
+            //    case "name":
+            //        //item = item.OrderByDescending(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                    break;
+            //    case "name_desc":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "numberSold_desc":
-                    //item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "numberSold":
 
-                case "priceBought":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //        break;
 
-                case "priceBought_desc":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "numberSold_desc":
+            //        //item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "priceSold":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "priceBought":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "priceSold_desc":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "priceBought_desc":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "diffInPrice":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "priceSold":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "diffInPrice_desc":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "priceSold_desc":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "revenue":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "diffInPrice":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                case "revenue_desc":
-                    item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
+            //    case "diffInPrice_desc":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
 
-                default:
-                    item = _context.Item.Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    //_context.Item.Include(i => i.Model).Include(i => i.ModelFromSupplier);
-                    break;
-            }
+            //    case "revenue":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
+
+            //    case "revenue_desc":
+            //        item = item.OrderBy(i => i.Name).Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
+
+            //    default:
+            //        item = _context.Item.Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        //_context.Item.Include(i => i.Model).Include(i => i.ModelFromSupplier);
+            //        break;
+            //}
 
             //int pageSize = 1;
             //return View(await PaginatedList<Item>.CreateAsync(item.AsNoTracking(), page ?? 1, pageSize));
-            return View(await item.ToListAsync());
+            //            return View(await item.ToListAsync());
         }
     }
 }
