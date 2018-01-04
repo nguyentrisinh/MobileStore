7c6ff929dc943e9311bddae183fa976cabfb6fc1
Unknown <nguyentrisinh0810@gmail.com>
2018-01-05 00:16:35 +0700

    Create Controller for Revenue Analytic

---
+++ MobileStore/Controllers/RevenueAnalyticController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MobileStore.Data;
+using MobileStore.Models;
+using MobileStore.Models.ApplicationUserViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using MobileStore.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace MobileStore.Controllers
+{
+    public class RevenueAnalyticController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public RevenueAnalyticController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context
+            )
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
