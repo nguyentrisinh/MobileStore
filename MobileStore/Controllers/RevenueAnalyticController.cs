ad20ecda4611fcb224ce22f233eb5e7d0a5544a9
Unknown <nguyentrisinh0810@gmail.com>
2018-01-06 02:32:12 +0700

    Done RevenueAnalytic but missing Dumpdata

--- MobileStore/Controllers/RevenueAnalyticController.cs
+++ MobileStore/Controllers/RevenueAnalyticController.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.Identity;
 using MobileStore.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.NodeServices;
+using Microsoft.EntityFrameworkCore;
+using MobileStore.Models.RevenueAnalyticViewModels;
 
 namespace MobileStore.Controllers
 {
@@ -37,6 +39,85 @@ namespace MobileStore.Controllers
 
         public IActionResult Index()
         {
+            //RevenueAnalyticViewModel revenueAnalyticViewModel = new RevenueAnalyticViewModel();
+            List<RevenueListViewModel> revenueListModel = new List<RevenueListViewModel>();
+            List<string> listDate = new List<string>();
+            List<double> totalSolds = new List<double>();
+            List<double> totalRevenues = new List<double>();
+            //var orders = from ent in _context.Order
+            //             select ent;
+
+            //var orders = _context.Set<Order>().Include("OrderDetails")
+            //    .Include("Item").Include("ModelFromSupplier");
+
+            var orders = _context.Order;
+                //.ThenInclude(ef => ef.Item)
+                //.ThenInclude(ef => ef.ModelFromSupplier);
+
+            var orderGrouped = from p in orders
+                               group p by new { month = p.Date.Month, year = p.Date.Year } into d
+                               select new {
+                                   month = d.Key.month,
+                                   year = d.Key.year,
+                                   dt = string.Format("{0}/{1}", d.Key.month, d.Key.year),
+                                   count = d.Count(),
+                                   //total = d.Sum(order => order.Total).ToString()
+                               };
+
+            //orderGrouped.ForEachAsync(group =>
+            //{
+            //    var orderList = _context.Order.Where(order => order.Date.Month == group.month && order.Date.Year == group.year);
+
+            //    var TotalSold = orderList.Sum(order => order.Total);
+
+            //    RevenueListViewModel revenueListViewModel = new RevenueListViewModel
+            //    {
+            //        Month = group.month,
+            //        Year = group.year,
+            //        Total = TotalSold
+            //    };
+
+            //    revenueAnalyticViewModel.RevenueListViewModels.Add(revenueListViewModel);
+            //});
+
+            foreach(var group in orderGrouped)
+            {
+                var orderList = _context.Order.Where(order => order.Date.Month == group.month && order.Date.Year == group.year);
+
+                var TotalSold = orderList.Sum(order => order.Total);
+
+                // Counting Revenue 
+                var orderListRevenue = _context.Order.Where(order => order.Date.Month == group.month && order.Date.Year == group.year).Include(order => order.OrderDetails)
+                    .ThenInclude(orderDetail => orderDetail.Item)
+                    .ThenInclude(item => item.ModelFromSupplier);
+
+                double TotalPrice = 0;
+
+                foreach(var order in orderListRevenue)
+                {
+                    TotalPrice += order.OrderDetails.Sum(orderDetail => orderDetail.Item.ModelFromSupplier.PriceBought);
+                }
+
+                RevenueListViewModel revenue = new RevenueListViewModel()
+                {
+                    Month = group.month,
+                    Year = group.year,
+                    Total = TotalSold,
+                    Revenue = TotalSold - TotalPrice
+                };
+
+                revenueListModel.Add(revenue);
+
+                listDate.Add(group.dt);
+                totalSolds.Add(TotalSold);
+                totalRevenues.Add(TotalSold - TotalPrice);
+            }
+
+            ViewData["RevenueList"] = revenueListModel;
+            ViewData["ListDate"] = listDate;
+            ViewData["TotalSold"] = totalSolds;
+            ViewData["TotalRevenue"] = totalRevenues;
+
             return View();
         }
     }
--- MobileStore/Models/RevenueAnalyticViewModels/RevenueAnalyticViewModel.cs
+++ MobileStore/Models/RevenueAnalyticViewModels/RevenueAnalyticViewModel.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 
 namespace MobileStore.Models.RevenueAnalyticViewModels
 {
+    public class RevenueListViewModel
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public double Total { get; set; }
+        public double Revenue { get; set; }
+    }
+
     public class RevenueAnalyticViewModel
     {
+        public IList<RevenueListViewModel> RevenueListViewModels { get; set; }
     }
 }
